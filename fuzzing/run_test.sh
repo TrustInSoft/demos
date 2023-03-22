95a09b3a84ac5033726e0880a873bf6b0bc53f23
okorach <olivier.korach@gmail.com>
2023-03-22 18:36:56 +0100

    Fix run_test with files as input

--- fuzzing/run_test.sh
+++ fuzzing/run_test.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-INPUT_FILE=input-files/seeds.txt
+INPUT_DIR=input-files
 
-if [ "$1" == "afl" ]; then
-	afl-fuzz -i input-files -o afl-results ./calculate 7 19
-elif [ "$1" == "loop" ]; then
-	for x in {-10..1000}; do
-		for y in {-10..1000}; do
-			./calculate $x $y
-		done
-	done
-else
-	nblines=$(cat $INPUT_FILE | wc -l)
-	echo "Seeds file has $nblines lines"
-	for line in {1..$nblines}; do
-		#echo "LIne = $line"
-		str=$(sed "${line}q;d" ${INPUT_FILE})
-		# echo "Running: calculate $str"
-		./calculate $line
-	done
-fi
+for file in $(ls input-files); do
+	echo "Running: ./calculate input-files/$file"
+	./calculate input-files/$file
+done
+
+exit 0
