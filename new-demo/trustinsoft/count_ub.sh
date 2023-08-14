2d4b7ce30d1955bea0fa287ac5f8ef1ee1c5be8e
okorach <olivier.korach@gmail.com>
2023-08-14 14:06:22 +0200

    Adjust for different cases of JSON structure

--- new-demo/trustinsoft/count_ub.sh
+++ new-demo/trustinsoft/count_ub.sh
@@ -3,4 +3,8 @@
 ME=$(basename $0)
 DIR="$(dirname $0)"
 
-cat $*|jq '{status: .alarms.status}'|grep -H '"status": "NOT OK"'|wc -l
+res=`cat $*|jq '{status: .alarms.list[].status}' 2>&1|grep -H -E "(NOT OK|unknown)"|wc -l`
+if [ "$res" = "" ]; then
+    res=`cat $*|jq '{status: .alarms.list.status}'|grep -H -E "(NOT OK|unknown)"|wc -l`
+fi
+echo $res
