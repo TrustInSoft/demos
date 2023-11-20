42808ffbe057051831594d268e7f32b957857581
Stephane Zimmermann <stephane.zimmermann@trust-in-soft.com>
2023-11-20 10:48:46 +0100

    [run_all] Double parentheses are favored instead of let expressions.
    Found by shellcheck.

--- tools/run_all.sh
+++ tools/run_all.sh
@@ -49,7 +49,7 @@ fi
 
 function run_analysis {
    analysis_nbr="$1"
-   let ndx=${analysis_nbr}-1
+   ndx=$(( analysis_nbr - 1 )) || true
    analysis_name=$(jq ".[$ndx][\"name\"]" < ${CONFIG_FILE} | cut -d '"' -f 2)
    if [ "${analysis_name}" = "null" ]; then
       analysis_name=${analysis_nbr}
