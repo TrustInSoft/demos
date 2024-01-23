2c4960635e4f26f0e67b0f5b6ba7109e455f6ee8
okorach <olivier.korach@gmail.com>
2024-01-23 02:08:03 +0100

    Remove flags

--- autosar/gen-drivers.sh
+++ autosar/gen-drivers.sh
@@ -1,1 +1,1 @@
-tis-autosar-mockup --arxml test.arxml --rte-header AP_Hello_World/Rte_AP_Hello_World.h --output-dir ".trustinsoft" -- -Irte_runtime -I. -IAP_Hello_World -DAUTOSAR_VERSION=51 -D__TIS_MACHDEP_X86_64
+tis-autosar-mockup --arxml test.arxml --rte-header AP_Hello_World/Rte_AP_Hello_World.h --output-dir ".trustinsoft" -- -Irte_runtime -I. -IAP_Hello_World 
